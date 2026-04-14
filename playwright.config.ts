521be89496493201f0837dbb44c9ce73a198d16b
yara-romero <yara.romero@assuresoft.com>
2026-04-14 09:03:57 -0400

    ci: enhance GitHub Actions pipeline and improve Playwright configuration stability

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,21 +2,19 @@ import { defineConfig, devices } from '@playwright/test';
 
 export default defineConfig({
   testDir: './tests',
+
+  outputDir: 'test-results/',
+
   reporter: [
     ['list'],
     ['html'],
     ['allure-playwright']
   ],
+
   use: {
     baseURL: 'https://www.saucedemo.com/',
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'retain-on-failure'
-  },
-  projects: [
-    {
-      name: 'Chromium',
-      use: { ...devices['Desktop Chrome'] }
-    }
-  ]
+  }
 });
